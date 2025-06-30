ce8892027655a14e1e7a1703d66dba9b9e2efa68
Omolola Owonikoko <omololaowonikoko@gmail.com>
2025-07-01 01:18:24 +0200

    update

--- world.sql
+++ world.sql
@@ -3,5 +3,21 @@
 
 -- select * from world;
 
-DROP TABLE IF EXISTS world;
 
+-- SELECT DATABASE();
+-- SHOW COLUMN;
+-- SELECT POPULATION FROM WORLD
+-- 	WHERE TRIM(Country) = 'Germany';
+-- -- DESC world;
+
+-- SELECT country, population FROM world
+-- 	WHERE TRIM(Country) IN ('Brazil', 'Sweden','Denmark');
+-- DESC world;
+-- SELECT Country, Area FROM  world
+-- 	WHERE TRIM(Area) BETWEEN 250000 AND 350000 ;
+-- SELECT Country, Population FROM world
+-- WHERE population >= 100000000;
+-- SELECT Country , GDP/population FROM world
+-- WHERE population >=100000000;
+SELECT Region FROM world
+ORDER BY Region ASC;
